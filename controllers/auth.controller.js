cf73bd49eaf7a45e0f8ee509a7cb8a11a6dd61fe
Baasir-stack <baasir121121@gmail.com>
2024-07-02 18:11:56 +0500

    change in logout api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -93,8 +93,8 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    // res.cookie("jwt", "", { maxAge: 0 });
-    res.clearCookie("jwt"); // Clear the cookie
+    res.cookie("jwt", "", { maxAge: 0, sameSite: "None" });
+    // res.clearCookie("jwt"); // Clear the cookie
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
