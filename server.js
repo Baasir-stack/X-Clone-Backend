46dbca0b33ec1ac0c32dc1df98832fe30d033ff5
Baasir-stack <baasir121121@gmail.com>
2024-07-02 16:22:24 +0500

    change db config

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectMongoDB();
+connectMongoDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
