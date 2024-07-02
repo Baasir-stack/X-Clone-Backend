bed97dc13072e5bf20866453336b498d2c6a62a9
Baasir-stack <baasir121121@gmail.com>
2024-07-02 15:07:10 +0500

    changes to static file

--- server.js
+++ server.js
@@ -24,21 +24,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-// app.use(express.static(path.join(__dirname, "../frontend/public")));
-// app.use(express.static(path.join(__dirname, "../frontend/public/avatars")));
-// app.use(express.static(path.join(__dirname, "../frontend/public/posts")));
-
 // CORS configuration
 app.use(
   cors({
-    origin: "https://x-clone-theta-six.vercel.app", // Replace with your frontend URL
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Replace with your frontend URL
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
 );
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
@@ -48,15 +43,9 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
