cb3b1d92f3b31eaeedd4ff8430dcc8b8ade85429
Baasir-stack <baasir121121@gmail.com>
2024-07-02 16:12:47 +0500

    a

--- server.js
+++ server.js
@@ -25,7 +25,18 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 // CORS configuration
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://x-clone-frontend-mu.vercel.app",
+      "https://x-clone-frontend-mu.vercel.app/",
+    ],
+
+    // Replace with your frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
